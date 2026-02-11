d119fb7ed8c505fb9c4975efe64f4206940c388c
Klaus Michalsky <k.michalsky81@googlemail.com>
2026-02-11 14:20:45 +0100

    renamed

--- I2C--Lectura_de_direccion/build/core/sdkoverride/btstack_run_loop_async_context.cpp.d
+++
@@ -1,2 +0,0 @@
-/Users/klausmichalsky/Proyectos/I2C/I2C--Lectura_de_direccion/build/core/sdkoverride/btstack_run_loop_async_context.cpp.o: \
- /Users/klausmichalsky/Library/Arduino15/packages/rp2040/hardware/rp2040/5.4.3/cores/rp2040/sdkoverride/btstack_run_loop_async_context.cpp
--- I2C--Lectura_de_direccion/build/core/sdkoverride/cyw43_arch_freertos.c.d
+++
@@ -1,2 +0,0 @@
-/Users/klausmichalsky/Proyectos/I2C/I2C--Lectura_de_direccion/build/core/sdkoverride/cyw43_arch_freertos.c.o: \
- /Users/klausmichalsky/Library/Arduino15/packages/rp2040/hardware/rp2040/5.4.3/cores/rp2040/sdkoverride/cyw43_arch_freertos.c
